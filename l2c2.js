edf92e33db26a38e5ade81e35dd1bb9929d96857
Sai Varshini Lattupally <b24cs263@kitsw.ac.in>
2026-08-05 16:08:49 +0530

    Add files via upload

--- l2c2.js
+++ l2c2.js
@@ -14,20 +14,4 @@ function updateZ(){
 }
 setInterval(updateX,500);
 setInterval(updateX,1000);
-setInterval(updateX,2000);var x=0,y=0,z=0;
-function displayValues(){
-    console.log("x=%d,y=%d,z=%d,x,y,z");
-}
-function updateX(){
-    x+=1;
-}
-function updateY(){
-    y+=1;
-}
-function updateZ(){
-    z+=1;
-    displayValues();
-}
-setInterval(updateX,500);
-setInterval(updateX,1000);
 setInterval(updateX,2000);

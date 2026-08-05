585cf84465160cda29779e20a8467c4883ac88a6
Sai Varshini Lattupally <b24cs263@kitsw.ac.in>
2026-08-05 15:39:33 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk'); 
+console.log(chalk.blue("=========================================")); 
+console.log(chalk.green(" NODE PACKAGE MANAGER (npm) DEMO ")); 
+console.log(chalk.blue("=========================================")); 
+console.log(chalk.yellow("Welcome to Full Stack Development Laboratory")); 
+console.log(chalk.green("Package Installed Successfully")); 
+console.log(chalk.magenta("Learning npm Packages in Node.js")); 
+console.log(chalk.red("Thank You!")); 
